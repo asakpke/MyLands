b4c3c9b9494bc5b559f3ab99e0dee7bc9f4de680
Aamir Shahzad <asakpke@gmail.com>
2018-09-23 04:34:00 +0500

    Disable Non Verified Command 2

--- src/Command/DisableNonVerifiedCommand.php
+++ src/Command/DisableNonVerifiedCommand.php
@@ -7,8 +7,20 @@ use Cake\Console\ConsoleIo;
 
 class DisableNonVerifiedCommand extends Command
 {
-    public function execute(Arguments $args, ConsoleIo $io)
-    {
-        $io->out('Hello world1.');
-    }
-}
+	public function initialize()
+	{
+		parent::initialize();
+		// $this->loadModel('Users');
+		$this->loadModel('Admins');
+	}
+
+	public function execute(Arguments $args, ConsoleIo $io)
+	{
+		$io->out('Hello world1.');
+		$admin = $this->Admins->get(3, [
+            'contain' => ['CostCats', 'LandStatuses', 'LandTypes', 'Lands']
+        ]);
+        // pr($admin);
+        pr($admin->name);
+	}
+} 
